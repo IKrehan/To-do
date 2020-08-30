4758acf63cb895e4184893d594073cf5b658a367
IKrehan <irvigkrehan@hotmail.com>
2020-08-30 16:58:10 -0300

    DB upadated

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'dp^%_(=)$ej^6o&z0s3e_c_dle(z%n_7^rrb@d@h7)6q&)a73n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangotodoproject.herokuapp.com']
+ALLOWED_HOSTS = ['djangotodoproject.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd29g4tthdnsrf',
+        'HOST': 'ec2-3-214-4-151.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'tudhjdtzgcdgpf',
+        'PASSWORD': 'acb405415e15133dd0e157e48663aef446585291d73ad6e088e0fd63f60b8015',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -122,12 +125,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-import django_heroku
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
